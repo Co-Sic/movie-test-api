758012e18e4089aa447edaf371c2a9b6a02ee3a1
sic <patrick-kl@web.de>
2020-05-25 18:23:40 +0200

    Update initialData

--- src/initialData.ts
+++ src/initialData.ts
@@ -1,4 +1,5 @@
 import {addMovie} from "./resolvers/movies";
+import {register} from "./resolvers/auth";
 
 interface MovieDefinition {
     name: string;
@@ -7,22 +8,54 @@ interface MovieDefinition {
     actors: string[];
 }
 
+interface UserDefinition {
+    username: string;
+    password: string;
+}
+
+const users: UserDefinition[] = [
+    {
+        username: "InitialUser1",
+        password: "password",
+    },
+];
+
 const movies: MovieDefinition[] = [
+    {
+        name: "Pulp Fiction",
+        durationSeconds: 154 * 60,
+        releaseDate: new Date("1994-10-14"),
+        actors: ["John Travolta", "Uma Thurman", "Samuel L. Jackson", "Bruce Willis"]
+    },
     {
         name: "Monty Python and the Holy Grail",
         durationSeconds: 5460,
         releaseDate: new Date("1976-09-13"),
-        actors: []
+        actors: ["Graham Chapman", "John Cleese", "Eric Idle"]
     },
     {
         name: "The Lord of the Rings: The Fellowship of the Ring",
         durationSeconds: 10680,
         releaseDate: new Date("2001-12-19"),
         actors: ["Elijah Wood", "Ian McKellen", "Orlando Bloom"]
+    },
+    {
+        name: "The Boondock Saints",
+        durationSeconds: 108 * 60,
+        releaseDate: new Date("1999-09-14"),
+        actors: ["Willem Dafoe", "Sean Patrick Flanery", "Norman Reedus"]
     }
 ];
 
 async function initData() {
+    for (let i = 0; i < users.length; i++) {
+        try {
+            await register(undefined, {...users[i]});
+        } catch (e) {
+            // error not relevant for data seeding
+        }
+    }
+
     for (let i = 0; i < movies.length; i++) {
         try {
             await addMovie(undefined, {...movies[i]});
@@ -30,6 +63,7 @@ async function initData() {
             // error not relevant for data seeding
         }
     }
+
 }
 
 export default initData;
